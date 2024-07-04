21fcb1a8c0b5b91a3901d643c01c57d9b7dd0a82
VIIIMaus <116817202+VIIIMaus@users.noreply.github.com>
2024-07-03 20:42:34 -0500

    Finished exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(numStart, numEnd) {
+    let sum = 0;
 
+    if(numStart > numEnd){
+        let aux = numEnd;
+        numEnd = numStart;
+        numStart = aux;
+    }
+
+    if(!(numStart < 0 || numEnd < 0) &&
+        (typeof numStart === "number" && typeof numEnd === "number")){
+        for(let i = numStart; i <= numEnd; i++){
+            sum += i;
+        }
+    
+        return sum;   
+    }
+
+    else return "ERROR";
 };
 
 // Do not edit below this line
